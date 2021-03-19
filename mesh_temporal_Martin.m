729a67b32a3a4d8dda2329f66e784039e0eee9bd
tonybkjr <77809974+tonybkjr@users.noreply.github.com>
2021-03-19 09:22:04 -0400

    Add files via upload

---
+++ mesh_temporal_Martin.m
@@ -0,0 +1,15 @@
+function t = mesh_temporal_Martin(t_tot,dt)
+%% Friday 11/11/2016
+%
+% This function 'mesh_temporal_Martin(t_tot,dt)' diescretizes the time domain from
+% 0-T_tot. 
+%
+%   Inputs: Total desired time to analyze temperature distribution and
+%   time step, dt. 
+%
+%   Outputs: Time vector, t.
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+t = 0:dt:t_tot;
+end
+
